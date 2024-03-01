806f2a9103ac7537e89babe6d034491bfcf34f78
wojrut <108264296+wojrut@users.noreply.github.com>
2024-03-01 11:15:41 +0100

    Add files via upload

---
+++ bloki_binarne.cpp
@@ -0,0 +1,42 @@
+﻿// Project6.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    fstream file;
+    file.open("bin.txt");
+    string s;
+    int wynik = 0;
+    while (!file.eof()) {
+        int licznik = 0;
+        file >> s;
+        if (file.eof()) break;
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != s[i + 1]) {
+                licznik++;
+            }
+        }
+
+        if (licznik <= 2) {
+            wynik++;
+        }
+    }
+
+    cout << wynik << endl;;
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
